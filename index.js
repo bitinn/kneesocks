12436f67efa956c8ff87be844b1e5023d270e665
David Frank <bitinn@gmail.com>
2015-01-11 19:02:15 +0800

    test cases and refactor

--- index.js
+++ index.js
@@ -6,102 +6,36 @@
  * A http proxy that wraps around your socks4/5 proxy
  */
 
-var Socks = require('socksv5');
-var Agent = Socks.HttpAgent;
-var HTTP = require('http');
-var URL = require('url');
+var Kneesocks = require('./lib/kneesocks');
 
-// command line arguments: kneesocks 10001 10002
-var args = process.argv.slice(2);
-var opts = {
-	http: 10001
-	, socks: 10002
+// command line mode
+if (!module.parent) {
+	var ks = factory(false);
+	ks.start();
+}
+
+/**
+ * Create a Kneesocks server instance
+ *
+ * @param   Mixed  opts  Config
+ * @return  Object
+ */
+function factory(opts) {
+
+	if (opts === false) {
+		// command line arguments, eg: kneesocks 10001 10002
+		var args = process.argv.slice(2);
+		opts = {};
+
+		if (args[0]) {
+			opts.http = args[0];
+		}
+
+		if (args[1]) {
+			opts.socks = args[1];
+		}
+	}
+
+	return new Kneesocks(opts);
+
 };
-
-if (args[0]) {
-	opts.http = args[0];
-}
-
-if (args[1]) {
-	opts.socks = args[1];
-}
-
-// agent with socks support
-var agent = new Agent({
-	proxyHost: '127.0.0.1'
-	, proxyPort: opts.socks
-	, auths: [ Socks.auth.None() ]
-});
-
-// proxy http requests
-var proxy = HTTP.createServer(function(req, res) {
-	console.log('start:', req.url);
-
-	// construct request using socks agent
-	var options = URL.parse(req.url);
-	options.method = req.method;
-	options.headers = req.headers;
-	options.agent = agent;
-
-	var request = HTTP.request(options, function(result) {
-		console.log('load:', req.url);
-		res.writeHead(result.statusCode, result.headers);
-
-		result.on('end', function() {
-			console.log('done:', req.url);
-		});
-		result.pipe(res);
-	});
-
-	request.on('error', function(err) {
-		console.log('error:', req.url, err.stack);
-	});
-
-	req.pipe(request);
-});
-
-// proxy https requests
-proxy.on('connect', function(req, inputSocket, head) {
-	// handle CONNECT, assume https
-	var options = URL.parse('https://' + req.url);
-
-	var url = 'https://' + options.hostname;
-	console.log('start:', url);
-
-	options = {
-		host: options.hostname
-		, port: options.port
-		, proxyHost: '127.0.0.1'
-		, proxyPort: opts.socks
-		, auths: [ Socks.auth.None() ]
-	};
-
-	var outputSocket = Socks.connect(options);
-
-	outputSocket.on('connect', function(socket) {
-		inputSocket.write('HTTP/1.1 200 Connection established.\r\n');
-		inputSocket.write('Proxy-Connection: close\r\n');
-		inputSocket.write('Proxy-Agent: Kneesocks\r\n');
-		inputSocket.write('\r\n');
-
-		socket.write(head);
-		console.log('load:', url);
-
-		socket.pipe(inputSocket);
-		inputSocket.pipe(socket);
-	});
-
-	outputSocket.on('error', function(err) {
-		console.log('error:', url, err.stack);
-	});
-
-	outputSocket.on('close', function() {
-		console.log('done:', url);
-	});
-});
-
-// start proxy
-proxy.listen(opts.http);
-
-console.log('http proxy listening on port:', opts.http);
-console.log('connected socks proxy on port:', opts.socks);
---
+++ test/test.js
@@ -0,0 +1,100 @@
+
+// test tools
+var chai = require('chai');
+var expect = chai.expect;
+
+// dependencies
+var http = require('http');
+var net = require('net');
+
+// fake socks proxy
+var Socks = require('socksv5');
+
+// test subject
+var Kneesocks = require('../lib/kneesocks.js');
+var proxy = new Kneesocks({ http: 10001, socks: 10002 });
+
+describe('kneesocks', function() {
+
+	before(function(done) {
+		var server = Socks.createServer(function(info, accept, deny) {
+			var body = 'hello world';
+			var socket = accept(true);
+
+			if (info.dstPort == 80) {
+				socket.write('HTTP/1.1 200 OK\r\n');
+				socket.write('Connection: close\r\n');
+				socket.write('Content-Type: text/plain\r\n');
+				socket.write('Content-Length: ' + Buffer.byteLength(body) + '\r\n');
+				socket.write(body + '\r\n');
+				socket.write('\r\n');
+				socket.end();
+			}
+		});
+		server.useAuth(Socks.auth.None());
+		server.listen(10002, '127.0.0.1', done);
+	});
+
+	describe('constructor', function() {
+		it('should setup http proxy server', function() {
+			expect(proxy.opts.http).to.equal(10001);
+			expect(proxy.opts.socks).to.equal(10002);
+			expect(proxy.agent).to.be.an.instanceof(Socks.HttpAgent);
+		});
+	});
+
+	describe('start', function() {
+		it('should start http proxy', function(done) {
+			proxy.start(function() {
+				expect(proxy.server).to.be.an.instanceof(http.Server);
+				expect(proxy.running).to.be.true;
+				done();
+			});
+		});
+	});
+
+	describe('stop', function() {
+		it('should stop http proxy', function(done) {
+			proxy.stop(function() {
+				expect(proxy.running).to.be.false;
+				done();
+			});
+		});
+	});
+
+	describe('proxy', function() {
+		it('should accept http request', function(done) {
+			this.timeout(5000);
+			proxy.start(function() {
+				var req = http.request({
+					host: '127.0.0.1'
+					, port: 10001
+					, method: 'GET'
+					, path: 'http://www.google.com/'
+				}, function(res) {
+					expect(res.statusCode).to.equal(200);
+					proxy.stop(function() {
+						done();
+					});
+				});
+				req.end();
+			});
+		});
+
+		it('should accept connect request', function(done) {
+			this.timeout(5000);
+			proxy.start(function() {
+				var req = http.request({
+					host: '127.0.0.1'
+					, port: 10001
+					, method: 'CONNECT'
+					, path: 'www.google.com:443'
+				}, function(res) {
+					console.log('fire');
+				});
+				req.end();
+			});
+		});
+	});
+
+});
