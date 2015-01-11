7a39b4ae18e8bc198a3b6e862a3dcb268475bc8f
David Frank <bitinn@gmail.com>
2015-01-11 14:34:37 +0800

    added https proxy support

--- index.js
+++ index.js
@@ -6,11 +6,10 @@
  * A http proxy that wraps around your socks4/5 proxy
  */
 
-var socks = require('socksv5');
-var Agent = socks.HttpAgent;
-var http = require('http');
-var TLS = require('tls');
-var url = require('url');
+var Socks = require('socksv5');
+var Agent = Socks.HttpAgent;
+var HTTP = require('http');
+var URL = require('url');
 
 // command line arguments: kneesocks 10001 10002
 var args = process.argv.slice(2);
@@ -31,17 +30,20 @@ if (args[1]) {
 var agent = new Agent({
 	proxyHost: '127.0.0.1'
 	, proxyPort: opts.socks
-	, auths: [ socks.auth.None() ]
+	, auths: [ Socks.auth.None() ]
 });
 
 // proxy http requests
-var server = http.createServer(function(req, res) {
-	var options = url.parse(req.url);
+var proxy = HTTP.createServer(function(req, res) {
+	console.log('start:', req.url);
+
+	// construct request using socks agent
+	var options = URL.parse(req.url);
 	options.method = req.method;
 	options.headers = req.headers;
 	options.agent = agent;
 
-	var proxy = http.request(options, function(result) {
+	var request = HTTP.request(options, function(result) {
 		console.log('load:', req.url);
 		res.writeHead(result.statusCode, result.headers);
 
@@ -51,25 +53,55 @@ var server = http.createServer(function(req, res) {
 		result.pipe(res);
 	});
 
-	proxy.on('error', function(err) {
+	request.on('error', function(err) {
 		console.log('error:', req.url, err.stack);
 	});
 
-	console.log('start:', req.url);
-	req.pipe(proxy);
+	req.pipe(request);
 });
 
 // proxy https requests
-server.on('connect', function(req, socket, head) {
-	socket.write('HTTP/1.1 200 Connection Established\r\n');
-	socket.write('Proxy-agent: Kneesocks\r\n');
+proxy.on('connect', function(req, inputSocket, head) {
+	// handle CONNECT, assume https
+	var options = URL.parse('https://' + req.url);
 
-	// x.pipe(inputSocket);
-	// inputSocket.pipe(x);
+	var url = 'https://' + options.hostname;
+	console.log('start:', url);
+
+	options = {
+		host: options.hostname
+		, port: options.port
+		, proxyHost: '127.0.0.1'
+		, proxyPort: opts.socks
+		, auths: [ Socks.auth.None() ]
+	};
+
+	var outputSocket = Socks.connect(options);
+
+	outputSocket.on('connect', function(socket) {
+		inputSocket.write('HTTP/1.1 200 Connection established.\r\n');
+		inputSocket.write('Proxy-Connection: close\r\n');
+		inputSocket.write('Proxy-Agent: Kneesocks\r\n');
+		inputSocket.write('\r\n');
+
+		socket.write(head);
+		console.log('load:', url);
+
+		socket.pipe(inputSocket);
+		inputSocket.pipe(socket);
+	});
+
+	outputSocket.on('error', function(err) {
+		console.log('error:', url, err.stack);
+	});
+
+	outputSocket.on('close', function() {
+		console.log('done:', url);
+	});
 });
 
 // start proxy
-server.listen(opts.http);
+proxy.listen(opts.http);
 
 console.log('http proxy listening on port:', opts.http);
 console.log('connected socks proxy on port:', opts.socks);
